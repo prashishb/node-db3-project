0769ec574046fb0f9270283556af894c005c32bc
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-08 14:43:24 -0500

    middleware complete

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,42 +1,61 @@
-/*
-  If `scheme_id` does not exist in the database:
+const Scheme = require('./scheme-model');
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const scheme = await Scheme.findById(req.params.scheme_id);
+    if (!scheme) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    } else {
+      req.scheme = scheme;
+      next();
+    }
+  } catch (err) {
+    next({
+      status: 500,
+      message: err.message,
+    });
   }
-*/
-const checkSchemeId = (req, res, next) => {
+};
 
-}
-
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
-
-}
-
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
+  if (
+    !req.body.scheme_name ||
+    typeof req.body.scheme_name !== 'string' ||
+    req.body.scheme_name.length === 0
+  ) {
+    next({
+      status: 400,
+      message: 'invalid scheme_name',
+    });
+  } else {
+    next();
   }
-*/
-const validateStep = (req, res, next) => {
+};
 
-}
+const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body;
+  if (
+    !instructions ||
+    instructions.length === 0 ||
+    typeof instructions !== 'string' ||
+    typeof step_number !== 'number' ||
+    isNaN(step_number) ||
+    step_number < 1
+  ) {
+    next({
+      status: 400,
+      message: 'invalid step',
+    });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
